0d4cc57aab9e444c4c713bd45ff1f0b2bf3beb8e
iva-a-a <ivanova.alena.a98@gmail.com>
2026-02-04 21:55:26 +0200

    удалены коментарии для документации

--- src/brick_game/struct.h
+++ src/brick_game/struct.h
@@ -8,86 +8,43 @@
 extern "C" {
 #endif
 
-/**
- * @enum UserAction_t
- * @brief пользовательское действие
- */
 typedef enum {
-  /** кнопка старт */
+
   Start = 10,
-  /** кнопка пауза */
   Pause,
-  /** кнопка выход */
   Terminate,
-  /** кнопка влево */
   Left,
-  /** кнопка вправо */
   Right,
-  /** кнопка вверх */
   Up,
-  /** кнопка вниз */
   Down,
-  /** кнопка движение */
   Action,
-  /** отсутствие нажатия нужной кнопки */
   None
 } UserAction_t;
 
-/**
- * @enum GameState_t
- * @brief состояния конечного автомата
- */
 typedef enum {
-  /** старт */
   Begin,
-  /** генерация */
   Generation,
-  /** падение фигуры или движение змейки*/
   Falling,
-  /** пауза */
   Break,
-  /** поворот */
   Moving_rotate,
-  /** движение влево */
   Moving_left,
-  /** движение вправо */
   Moving_right,
-  /** движение вниз */
   Moving_down,
-  /** соединение */
   Attaching,
-  /** конец игры */
   End,
-  /** выход из игры */
   Exit
 } GameState_t;
 
-/**
- * @struct GameInfo_t
- * @brief информация о текущем состоянии игры
- */
 typedef struct {
-  /** игровое поле */
   int **field;
-  /** превью */
   int **next;
-  /** количество очков */
   int score;
-  /** количество рекордных очков */
   int high_score;
-  /** номер уровня */
   int level;
-  /** скорость */
   int speed;
-  /** нажата ли пауза */
   int pause;
 } GameInfo_t;
 
-/**
- * @brief получение времени в миллисекундах
- *
- * @return возвращает время в миллисекундах
- */
 static inline long long int time_in_millisec(void) {
   struct timeval tv;
   gettimeofday(&tv, NULL);
