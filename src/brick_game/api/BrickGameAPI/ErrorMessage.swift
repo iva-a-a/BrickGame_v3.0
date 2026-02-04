582ef43b6dbe501d6d2b0deb55c34b4f079ec87d
iva-a-a <ivanova.alena.a98@gmail.com>
2026-02-04 14:38:36 +0200

    перенесен ErrorMessage в отдельный модуль моделей и добавлены расширения моделей для сервера

---
+++ src/brick_game/api/BrickGameAPI/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  Server
+//
+//  Created by Alena Ivanova on 19.01.2026.
+//
+
+import Foundation
+
+public struct ErrorMessage: Codable, Sendable {
+    public let message: String
+    
+    public init(message: String) {
+        self.message = message
+    }
+}
---
+++ src/brick_game/server/Sources/Server/Extensions/ModelsExtendions.swift
@@ -0,0 +1,15 @@
+//
+//  ModelsExtendions.swift
+//  Server
+//
+//  Created by Alena Ivanova on 04.02.2026.
+//
+
+import Vapor
+import BrickGameAPI
+
+extension ErrorMessage: Content {}
+extension GameInfo: Content {}
+extension GameList: Content {}
+extension GameState: Content {}
+extension UserAction: Content {}
