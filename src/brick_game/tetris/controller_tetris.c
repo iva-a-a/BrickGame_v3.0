49918d6b3b4e0a726c02f514c87fc595b8f89868
iva-a-a <ivanova.alena.a98@gmail.com>
2026-02-09 23:08:50 +0200

    изменена запись в field и next

--- src/brick_game/tetris/controller_tetris.c
+++ src/brick_game/tetris/controller_tetris.c
@@ -1,9 +1,13 @@
 #include "controller_tetris.h"
 
 void tetris_userInput(UserAction_t action, bool hold) {
-    (void)hold;
-    Game_tetris *tetris = get_ptr_game_tetris();
-    fsm(tetris, action);
+  (void)hold;
+  Game_tetris *tetris = get_ptr_game_tetris();
+  if (!tetris->is_init) {
+    setup_game(tetris);
+    tetris->is_init = true;
+  }
+  fsm(tetris, action);
 }
 
 GameInfo_t tetris_updateCurrentState() {
@@ -16,17 +20,17 @@ GameInfo_t tetris_updateCurrentState() {
 
   update_game(tetris);
 
-  free_matrix(tetris->render_field, ROWS_BOARD);
-  tetris->render_field = build_render_field(tetris);
+  build_render_field_inplace(tetris);
 
   GameInfo_t info = (GameInfo_t){0};
   info.field = tetris->render_field;
-  info.next = tetris->next;
   info.score = tetris->score;
   info.high_score = tetris->high_score;
   info.level = tetris->level;
   info.speed = tetris->speed;
   info.pause = (tetris->state == Break);
+  info.next = (tetris->state == End) ? NULL : tetris->next;
 
   return info;
 }
+
