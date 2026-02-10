7a95efd6464509bb65209b60007cdbc60580cf84
iva-a-a <ivanova.alena.a98@gmail.com>
2026-02-10 20:37:10 +0200

    из контроллера удалены ненужные методы

--- src/brick_game/snake/controller_snake.h
+++ src/brick_game/snake/controller_snake.h
@@ -1,21 +1,19 @@
 #pragma once
 
 #include "model_snake.h"
+#include "highscore_storage.h"
 
 class Controller {
 public:
-    Controller() = default;
+    Controller() : storage_(), model_(&storage_) {}
     ~Controller() = default;
     
     void userInput(UserAction_t currentAction, bool hold);
     GameInfo_t updateCurrentState();
-    void clearGameInfo(GameInfo_t &info_snake);
     
-    SnakeGame *get_model();
+    void clearGameInfo(GameInfo_t& info);
+
 private:
-    SnakeGame model;
-    
-    int **convert_snake_to_array(std::list<Coordinate> snake);
-    int **convert_apple_to_array(Coordinate apple);
-    void free_array(int **array);
+    HighScoreStorage storage_;
+    SnakeGame model_;
 };
