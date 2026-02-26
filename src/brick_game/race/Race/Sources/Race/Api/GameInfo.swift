2325ce0e72d94c395735dfdd9cd14eb190561ae3
satvajos <satvajos@student.21-school.ru>
2026-02-26 15:03:26 +0200

    удалена инициализация GameInfo

--- src/brick_game/race/Race/Sources/Race/Api/GameInfo.swift
+++ src/brick_game/race/Race/Sources/Race/Api/GameInfo.swift
@@ -6,18 +6,11 @@
 //
 
 struct GameInfo {
-  var field: [[Bool]] = Array(
-    repeating: Array(repeating: false, count: GameConstants.Field.columns),
-    count: GameConstants.Field.rows
-  )
-  var next: [[Bool]] = Array(
-    repeating: Array(repeating: false, count: GameConstants.Field.columns),
-    count: GameConstants.Field.rows
-  )
-
-  var score: Int = 0
-  var highScore: Int = 0
-  var level: Int = 1
-  var speed: Int = 1
-  var pause: Bool = false
+  let field: [[Bool]]
+  let next: [[Bool]]
+  let score: Int
+  let highScore: Int
+  let level: Int
+  let speed: Int
+  let pause: Bool
 }
