8845bdbd0f019a6c33320d5be39bcf331c6f6f77
satvajos <satvajos@student.21-school.ru>
2026-02-26 15:02:39 +0200

    перенесен файл

---
+++ src/brick_game/race/Race/Sources/Race/Engine/RaceGameState.swift
@@ -0,0 +1,19 @@
+//
+//  RaceGameState.swift
+//  Race
+//
+//  Created by Alena Ivanova on 11.12.2025.
+//
+
+import Playgrounds
+
+enum RaceGameState {
+    case begin
+    case generation
+    case running
+    case `break`
+    case movingLeft
+    case movingRight
+    case end
+    case exit
+}
