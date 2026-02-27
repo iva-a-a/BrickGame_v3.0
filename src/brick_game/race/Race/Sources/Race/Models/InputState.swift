1a120e9f9b2ec6309a496ddb0e3671bd1514baa5
satvajos <satvajos@student.21-school.ru>
2026-02-27 11:52:43 +0200

    добавлен InputState

--- src/brick_game/race/Race/Sources/Race/Models/InputState.swift
+++ src/brick_game/race/Race/Sources/Race/Models/InputState.swift
@@ -1,7 +1,13 @@
 //
-//  Untitled.swift
+//  InputState.swift
 //  Race
 //
 //  Created by Alena Ivanova on 26.02.2026.
 //
 
+import Foundation
+
+struct InputState {
+    var action: Action = .none
+    var hold: Bool = false
+}
