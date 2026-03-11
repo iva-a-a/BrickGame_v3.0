8f7dbe6ae5dd290eaca0f8e594f05c73341841bb
satvajos <satvajos@student.21-school.ru>
2026-03-11 16:51:10 +0200

    добавлена функция очистки игрового мира

--- src/brick_game/race/Sources/Models/RaceWorld.swift
+++ src/brick_game/race/Sources/Models/RaceWorld.swift
@@ -8,16 +8,24 @@
 struct RaceWorld {
     var player: Car
     var enemies: [Car]
-    
+
     init() {
-        self.player = Car(
-            upLeftPosition:
-                Coordinate(
-                    x: (GameConstants.Field.columns - CarShapes.size.w) / 2,
-                    y: GameConstants.Field.rows - CarShapes.size.h - 1
-                ),
+        self.player = RaceWorld.initialPlayerCar()
+        self.enemies = []
+    }
+    
+    mutating func clear() {
+        player = RaceWorld.initialPlayerCar()
+        enemies.removeAll()
+    }
+
+    private static func initialPlayerCar() -> Car {
+        let startX = (GameConstants.Field.columns - CarShapes.size.w) / 2
+        let startY = GameConstants.Field.rows - CarShapes.size.h - 1
+        
+        return Car(
+            upLeftPosition: Coordinate(x: startX, y: startY),
             body: CarShapes.player
         )
-        self.enemies = []
     }
 }
