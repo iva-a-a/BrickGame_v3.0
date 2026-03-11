9ed0c92e470c7385f65a2050d4bc79376fcb2480
satvajos <satvajos@student.21-school.ru>
2026-03-11 21:55:55 +0200

    изменен вызов функций для гонок

--- src/brick_game/server/Sources/GameCore/RaceEngine.swift
+++ src/brick_game/server/Sources/GameCore/RaceEngine.swift
@@ -7,17 +7,15 @@
 
 import BrickGameAPI
 import RaceSwiftLib
-import TetrisCLib
 
 struct RaceEngine: BrickGameEngine {
-    private let game = RaceGame()
 
     func userInput(actionId: Int, hold: Bool) {
-        game.userInput(action: UserAction_t(UInt32(actionId)), hold: hold)
+        race_userInput(UInt32(actionId), hold)
     }
 
     func getState() -> GameState {
-        let info = game.updateCurrentState()
+        let info = race_updateCurrentState()
         return BrickGameStateMapper.map(info: info)
     }
 }
