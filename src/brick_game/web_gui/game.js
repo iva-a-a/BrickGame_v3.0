c924d9a8465b2929773b85edc3c1202f4e69dfb1
satvajos <satvajos@student.21-school.ru>
2026-03-18 20:16:09 +0200

    добавлена инициализация игры и обработка кнопок

--- src/brick_game/web_gui/game.js
+++ src/brick_game/web_gui/game.js
@@ -1,34 +1,63 @@
 import { applyRootStyles } from './src/utils.js';
 import { GameBoard } from './src/game-board.js';
 import { rootStyles, keyCodes } from './src/config.js';
+import { getGames, selectGame, postAction } from './src/api.js';
+import { GamePanel } from './src/game-panel.js';
 
 applyRootStyles(rootStyles);
 const gameBoard = new GameBoard(document.querySelector('#game-board'));
+const panel = new GamePanel(document.querySelector('#side-panel'));
 
 const $sidePanel = document.querySelector('#side-panel');
 
-document.addEventListener('keydown', function (event) {
-    
-//    start: ['Enter'],
-//    pause: ['KeyP'],
-//    terminate: ['Escape'],
-//    up: ['ArrowUp'],
-//    right: ['ArrowRight'],
-//    down: ['ArrowDown'],
-//    left: ['ArrowLeft'],
-//    action: ['Space'],
-    if (keyCodes.up.includes(event.code)) {
-        gameBoard.enableTile(4, 5);
-        console.log('up');
+document.addEventListener('keydown', async function (event) {
+    if (keyCodes.start.includes(event.code)) {
+        await postAction(10, false);
+        console.log('start');
     }
-    if (keyCodes.right.includes(event.code)) {
-        gameBoard.disableTile(4, 5);
-        console.log('right');
+    if (keyCodes.pause.includes(event.code)) {
+        await postAction(11, false);
+        console.log('pause');
     }
-    if (keyCodes.down.includes(event.code)) {
-        console.log('down');
+    if (keyCodes.terminate.includes(event.code)) {
+        await postAction(12, false);
+        console.log('terminate');
     }
     if (keyCodes.left.includes(event.code)) {
+        await postAction(13, false);
         console.log('left');
     }
+    if (keyCodes.right.includes(event.code)) {
+        await postAction(14, false);
+        console.log('right');
+    }
+    if (keyCodes.up.includes(event.code)) {
+        await postAction(15, false);
+        console.log('up');
+    }
+    if (keyCodes.down.includes(event.code)) {
+        await postAction(16, false);
+        console.log('down');
+    }
+    if (keyCodes.action.includes(event.code)) {
+        await postAction(17, false);
+        console.log('action');
+    }
 });
+
+async function initGames() {
+    try {
+        const data = await getGames();
+        const games = data.games;
+
+        panel.render(games, async (game) => {
+            console.log('Selected:', game.id);
+            await selectGame(game.id);
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+// Запускаем
+initGames();
