66d38536b4c8f20973b45725dfb4217833e1a7f2
satvajos <satvajos@student.21-school.ru>
2026-03-20 14:38:10 +0200

    изменена начальная скорость

--- src/brick_game/race/Sources/Models/RaceStats.swift
+++ src/brick_game/race/Sources/Models/RaceStats.swift
@@ -9,14 +9,14 @@ struct RaceStats {
     var score: Int = 0
     var highScore: Int = 0
     var level: Int = 1
-    var speed: Int = 1000
+    var speed: Int = GameConstants.Gameplay.baseSpeedMs
     var pause: Bool = false
     
     mutating func clear() {
         self.score = 0
         self.highScore = 0
         self.level = 1
-        self.speed = 1000
+        self.speed = GameConstants.Gameplay.baseSpeedMs
         self.pause = false
     }
 }
